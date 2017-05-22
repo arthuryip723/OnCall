49ee54395b5cab0b258d757a460c9d025ca63695
Arthur Yip <arthuryip723@gmail.com>
2017-05-21 21:12:55 -0400

    built some mock-up data of peopl

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,1 +1,5 @@
 var onCallApp = angular.module('onCallApp', []);
+
+onCallApp.controller('BabeListController', function BabeListController($scope) {
+  $scope.babes = [{name: 'babe1'}, {name: 'babe2'}];
+});
