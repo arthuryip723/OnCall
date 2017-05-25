fa5e02acfcb22541e784adcde2e80e9d75ae1aa7
Arthur Yip <arthuryip723@gmail.com>
2017-05-24 21:45:08 -0400

    added some comments

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,5 +1,8 @@
+// This looks like some registration for the angular.
 var onCallApp = angular.module('onCallApp', []);
 
 onCallApp.controller('BabeListController', function BabeListController($scope) {
+  // Build the rails back-end to offer an API
+  // Make a request by using the $http service.
   $scope.babes = [{name: 'babe1'}, {name: 'babe2'}];
 });
